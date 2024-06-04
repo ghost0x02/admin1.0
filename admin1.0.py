5638b78a2ab170cbab95bda690579246b2bb01a7
enesxsec <120325383+ghost0x02@users.noreply.github.com>
2024-06-04 21:59:38 +0300

    Update admin1.0.py

--- admin1.0.py
+++ admin1.0.py
@@ -2,7 +2,6 @@ import requests
 from colorama import Fore, Style
 import time
 import os
-import socket
 
 admin_paths = [
     "/admin",
@@ -32,7 +31,7 @@ admin_paths = [
 
 def check_url(url):
     """
-    Panel kontrolü
+    Check if the panel exists
     """
     try:
         response = requests.get(url, timeout=5)
@@ -45,37 +44,38 @@ def check_url(url):
 
 def find_admin_panel(base_url):
     """
-    Admin Panel.
+    Find admin panel.
     """
     if not base_url.startswith("http://") and not base_url.startswith("https://"):
         base_url = "http://" + base_url
     time.sleep(1)
     print(Fore.YELLOW + "SCANNING...\n")
+    found_panels = []
     for path in admin_paths:
         full_url = base_url.rstrip("/") + path
-        print(Fore.CYAN + f"Kontrol ediliyor: {full_url}")
+        print(Fore.CYAN + f"Checking: {full_url}")
         if check_url(full_url):
-            print(Fore.GREEN + Style.BRIGHT + f"【+】Admin paneli bulundu: {full_url}\n")
+            print(Fore.GREEN + Style.BRIGHT + f"【+】Admin panel found: {full_url}\n")
+            found_panels.append(full_url)
         else:
-            print(Fore.RED + Style.BRIGHT + f"【-】Admin paneli bulunamadı: {full_url}\n")
+            print(Fore.RED + Style.BRIGHT + f"【-】Admin panel not found: {full_url}\n")
         time.sleep(2.5)
+    return found_panels
 
 def main():
     time.sleep(1)
     print(Fore.YELLOW + Style.BRIGHT + "SCANNING..." + Style.RESET_ALL) 
-
     print(Fore.MAGENTA + "")
     os.system("clear")
     os.system("figlet admin")
-    input("Programı çalıştırmak için enter tuşuna basınız...")
+    input("Press enter to run the program...")
     time.sleep(1)
     print(Fore.GREEN + "")
-    base_url = input("【+】Hedef site girin (HTTPS/HTTP): ").strip()
+    base_url = input("【+】Enter target site (HTTPS/HTTP): ").strip()
     time.sleep(1)
     os.system("clear")
     print(Style.RESET_ALL)
     print(Fore.RED + Style.BRIGHT + "")
-
     print("""
 
 THIS SOFTWARE IS FOR TESTING!!!
@@ -83,6 +83,7 @@ THIS SOFTWARE IS FOR TESTING!!!
 producer  〔coded by enesxsec〕
 instagram 〔xsecit〕
 github    〔https://github.com/ghost0x02〕
+
 
 ███████████████████████████
 ███████▀▀▀░░░░░░░▀▀▀███████
@@ -102,10 +103,19 @@ github    〔https://github.com/ghost0x02〕
 █████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████
 ███████▄░░░░░░░░░░░▄███████
 ██████████▄▄▄▄▄▄▄██████████
-███████████████████████████""")
+███████████████████████████
 
 
-    find_admin_panel(base_url)
+""")
+
+
+    found_panels = find_admin_panel(base_url)
+    if found_panels:
+        print(Fore.YELLOW + "Found admin panels:")
+        for panel in found_panels:
+            print(panel)
+    else:
+        print("No admin panels found.")
 
 if __name__ == "__main__":
     main()
